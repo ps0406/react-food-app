f697e45ecd6b8e912b123393d67f5eefb2cd7a8a
Pei Sin Yap <peisin.yap@growthops.asia>
2023-09-16 00:43:03 +0800

    Dev: <MealItem> - added meal item form into meal item component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,18 +1,19 @@
-import classes from "./MealItem.module.css"
+import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 export default (props) => {
-const price = props.price ? `$${props.price?.toFixed(2)}` : '$0' ;
+	const price = props.price ? `$${props.price?.toFixed(2)}` : '$0';
 
-  return (
-    <li className={classes.meal}>
-      <div>
-        <h3>{props.name}</h3>
-        <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
-      </div>
-      <div>
-
-      </div>
-    </li>
-  );
+	return (
+		<li className={classes.meal}>
+			<div>
+				<h3>{props.name}</h3>
+				<div className={classes.description}>{props.description}</div>
+				<div className={classes.price}>{price}</div>
+			</div>
+			<div>
+				<MealItemForm/>
+			</div>
+		</li>
+	);
 };
