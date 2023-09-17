17c1b148f51ac2a04a7e0513021b50968286f69a
Pei Sin Yap <peisin.yap@growthops.asia>
2023-09-17 19:02:38 +0800

    Dev: <Input> - added ref to input to refer correct item in cart context

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,17 @@
+import { forwardRef } from 'react';
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = forwardRef(function Input(props, ref) {
 	return (
 		<div className={classes.input}>
 			<label htmlFor={props.input.id}>{props.label}</label>
-			<input id={props.input.id} {...props.input}/>
+			<input
+				ref={ref}
+				id={props.input.id}
+				{...props.input}
+			/>
 		</div>
 	);
-};
+});
 
 export default Input;
