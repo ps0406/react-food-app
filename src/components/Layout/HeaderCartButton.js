629cff66c5a18c699058621ba951b954cbd4f0ec
Pei Sin Yap <peisin.yap@growthops.asia>
2023-09-17 19:00:14 +0800

    Dev: <HeaderCartButton> - get number of items from cart context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,24 @@
-import classes from "./HeaderCartButton.module.css";
-import logo from "../../assets/svg/cart-icon.svg";
+import classes from './HeaderCartButton.module.css';
+import logo from '../../assets/svg/cart-icon.svg';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 
 export default (props) => {
- 
-  return (
-    <button className={classes.button} onClick={props.onClick}>
-      <span className={classes.icon}>
-        <img src={logo} alt="cart-logo"></img>
-      </span>
-      <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
-    </button>
-  );
+	const cartItem = useContext(CartContext);
+	const numberOfItem = cartItem.items.reduce((curNumber, item) => {
+		return curNumber + item.amount;
+	}, 0);
+	return (
+		<button
+			className={classes.button}
+			onClick={props.onClick}>
+			<span className={classes.icon}>
+				<img
+					src={logo}
+					alt='cart-logo'></img>
+			</span>
+			<span>Your Cart</span>
+			<span className={classes.badge}>{numberOfItem}</span>
+		</button>
+	);
 };
