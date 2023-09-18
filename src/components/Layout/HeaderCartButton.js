8e3eaca3d8a133d1d99a295facde1d7b6a71e6ae
Pei Sin Yap <peisin.yap@growthops.asia>
2023-09-18 12:37:00 +0800

    Style: <HeaderCartButton> - updated header cart button styling

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,24 +1,39 @@
 import classes from './HeaderCartButton.module.css';
 import logo from '../../assets/svg/cart-icon.svg';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 export default (props) => {
 	const cartItem = useContext(CartContext);
+	const [buttonIsAnimated, setButtonIsAnimated] = useState(false);
 	const numberOfItem = cartItem.items.reduce((curNumber, item) => {
 		return curNumber + item.amount;
 	}, 0);
+	const { items } = cartItem;
+	useEffect(() => {
+		if (items.length > 0) setButtonIsAnimated(true);
+		const timer = setTimeout(() => {
+			setButtonIsAnimated(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
 	return (
 		<button
-			className={classes.button}
+			className={`${classes.button} ${buttonIsAnimated && classes.bump}`}
 			onClick={props.onClick}>
-			<span className={classes.icon}>
+			<div className={classes.icon}>
 				<img
 					src={logo}
 					alt='cart-logo'></img>
-			</span>
-			<span>Your Cart</span>
-			<span className={classes.badge}>{numberOfItem}</span>
+
+				<div className={classes.badge}>
+					<span>{numberOfItem}</span>
+				</div>
+			</div>
+			<span>Cart</span>
 		</button>
 	);
 };
